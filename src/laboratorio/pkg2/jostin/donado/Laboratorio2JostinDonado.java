c372a44ebd522107d96a152d56ac47636bf37994
joslim927 <jdonadol1@miumg.edu.gt>
2026-04-18 22:18:20 -0600

    Primer commit

---
+++ src/laboratorio/pkg2/jostin/donado/Laboratorio2JostinDonado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio.pkg2.jostin.donado;
+
+/**
+ *
+ * @author umg
+ */
+public class Laboratorio2JostinDonado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
